7819a169d5eaa95474b9e048811b8cf95dd19c61
heeryung <heeryung.c@gmail.com>
2016-07-27 20:31:50 +0900

    set

---
+++ main.py
@@ -0,0 +1,37 @@
+from konlpy.tag import Kkma
+from konlpy.utils import pprint
+
+#most frequent one
+import konlpy
+from konlpy.tag import Hannanum
+from konlpy.utils import pprint
+from collections import Counter
+
+
+#Kkma or Hannanum or Mecab
+# def textinput (input):
+#     kkma = Kkma()
+#     pprint(kkma.pos(input)
+#
+#
+
+
+def frequency (text_input):
+    mecab = Mecab()
+    text_input = input('Please enter a sentence: ')
+    input_list = mecab.morphs(text_input)) #list
+    #morphs?
+    
+    value = []
+    for inpulist[i] in input_list :
+        if dic[i] :
+            value[i] = dic[i]
+        
+        else 
+            novalue[i] = i
+    
+    
+    
+    
+
+    
